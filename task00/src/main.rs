f69d5d6e93b824a8ac7c140106d126c69d81b66c
Lixin Xiu <lixin_xiu@outlook.com>
2025-04-23 19:24:57 +0900

    Lixin Xiu finished task00

--- task00/src/main.rs
+++ task00/src/main.rs
@@ -58,13 +58,20 @@ fn problem1() -> anyhow::Result<()> {
         
         // modify the defninition of p2
         let p2 = [
-            p1[0],
-            p1[1],
+            p1[0] + 50f32 * (2f32 * radian).cos(),
+            p1[1] - 50f32 * (2f32 * radian).sin(),
         ];
         del_canvas::rasterize::line::draw_dda(&mut canvas.data, canvas.width, &p1, &p2, 2);
         trajectory.push(p2); // hint
         // draw trajectoty using for loop below.
-
+        for i in 1..trajectory.len() {
+            del_canvas::rasterize::line::draw_dda(&mut canvas.data, 
+                canvas.width, 
+                &trajectory[i - 1], 
+                &trajectory[i],
+                3,
+            );
+        }
         // ---------------------
         // no further edit from here
         canvas.write(); // save current frame
